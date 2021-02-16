fd8769fbff3af457abba8a5afe3a824960652738
Emisar <33164788+Emisar@users.noreply.github.com>
2021-02-16 20:10:16 +0400

    add Dockerfiles and docker-compose add some setup workspace-api and employees-api application

--- business-services/employees-api/src/main/groovy/com/epam/hw/netflix/EmployeesApplication.groovy
+++ business-services/employees-api/src/main/groovy/com/epam/hw/netflix/EmployeesApplication.groovy
@@ -2,7 +2,9 @@ package com.epam.hw.netflix
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableEurekaClient
 @SpringBootApplication
 class EmployeesApplication {
     static void main(String[] args) {
